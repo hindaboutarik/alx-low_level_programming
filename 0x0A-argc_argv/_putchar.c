da42765a5f367c41194743bc292c67defacd092a
hindaboutarik <hindaboutarik.dev@gmail.com>
2023-05-13 08:03:40 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_putchar - writes the character c to stdout
+ *@c: ther caracter to print
+ * Return: On Success 1.
+ * On error, -1 is returned and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
